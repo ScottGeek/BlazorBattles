441d7f885ae4bb84ae1c4253616347711f8f0cd8
ScottGeek <ScottGeek@users.noreply.github.com>
2021-07-29 16:46:40 -0400

    We have Bananas working

--- BlazorBattles/Client/Program.cs
+++ BlazorBattles/Client/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using BlazorBattles.Client.Services;
 
 namespace BlazorBattles.Client
 {
@@ -18,6 +19,8 @@ namespace BlazorBattles.Client
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            builder.Services.AddScoped<IBananaService, BananaService>();
 
             await builder.Build().RunAsync();
         }
