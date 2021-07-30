66f38d78e73d33b3d4371b744f45a1b4922d43bc
ScottGeek <ScottGeek@users.noreply.github.com>
2021-07-30 17:20:40 -0400

    Added first api call to get units.

--- BlazorBattles/Client/Services/UnitService.cs
+++ BlazorBattles/Client/Services/UnitService.cs
@@ -4,24 +4,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Blazored.Toast.Services;
+using System.Net.Http;
+using System.Net.Http.Json;
 
 namespace BlazorBattles.Client.Services
 {
     public class UnitService : IUnitService
     {
         private readonly IToastService _toastService;
+        private readonly HttpClient _httpClient;
 
-        public UnitService(IToastService toastService)
+        public UnitService(IToastService toastService, HttpClient httpClient)
         {
             _toastService = toastService;
+            _httpClient = httpClient;
         }
 
-        public IList<Unit> Units => new List<Unit>
-        {
-            new Unit { Id =1, Title = "Knight", Attack=10, BananaCost=100, Defense=10},
-            new Unit { Id =2, Title = "Archer", Attack=15, BananaCost=150, Defense=5},
-            new Unit { Id =3, Title = "Mage", Attack=20, BananaCost=200, Defense=1},
-        };
+        public IList<Unit> Units { get; set; } = new List<Unit>();
+
         public IList<UserUnit> MyUnits { get; set; } = new List<UserUnit>();
 
         public void AddUnit(int unitId)
@@ -33,6 +33,14 @@ namespace BlazorBattles.Client.Services
 
             //Console.WriteLine($"{unit.Title} was built!");
             //Console.WriteLine($"Your army size: {MyUnits.Count}");
+        }
+
+        public async Task LoadUnitAsync()
+        {
+            if (Units.Count == 0)
+            {
+                Units = await _httpClient.GetFromJsonAsync<IList<Unit>>("api/Unit");
+            }
         }
     }
 }
