66152571d2b6caecc0cd217fe45a3c03ddd0facc
ScottGeek <ScottGeek@users.noreply.github.com>
2021-08-02 16:00:40 -0400

    Added Unit EF and Table

--- BlazorBattles/Server/Controllers/UnitController.cs
+++ BlazorBattles/Server/Controllers/UnitController.cs
@@ -1,9 +1,12 @@
-﻿using BlazorBattles.Shared;
+﻿using BlazorBattles.Server.Data;
+using BlazorBattles.Shared;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic.CompilerServices;
 
 namespace BlazorBattles.Server.Controllers
 {
@@ -11,18 +14,77 @@ namespace BlazorBattles.Server.Controllers
     [ApiController]
     public class UnitController : ControllerBase
     {
-        public IList<Unit> Units => new List<Unit>
+        private readonly DataContext _context;
+
+        #region Old Hardcoded before db added
+        //public IList<Unit> Units => new List<Unit>
+        //{
+        //    new Unit { Id =1, Title = "Knight", Attack=10, BananaCost=100, Defense=10},
+        //    new Unit { Id =2, Title = "Archer", Attack=15, BananaCost=150, Defense=5},
+        //    new Unit { Id =3, Title = "Mage", Attack=20, BananaCost=200, Defense=1},
+        //}; 
+        #endregion
+
+        public UnitController(DataContext context)
         {
-            new Unit { Id =1, Title = "Knight", Attack=10, BananaCost=100, Defense=10},
-            new Unit { Id =2, Title = "Archer", Attack=15, BananaCost=150, Defense=5},
-            new Unit { Id =3, Title = "Mage", Attack=20, BananaCost=200, Defense=1},
-        };
+            _context = context;
+        }
 
         [HttpGet]
         public async Task<IActionResult> GetUnits()
         {
-            return Ok(Units);
+
+            var units = await _context.Units.ToListAsync();
+            return Ok(units);
         }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUnit(Unit unit)
+        {
+            _context.Units.Add(unit);
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Units.ToListAsync());
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUnit(int id, Unit unit)
+        {
+            var result = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (result == null)
+            {
+                return NotFound("Unit with the given Id does not exits");
+            }
+
+            result.Title = unit.Title;
+            result.Defense = unit.Defense;
+            result.Attack = unit.Attack;
+            result.BananaCost = unit.BananaCost;
+            result.HitPoints = unit.HitPoints;
+
+            await _context.SaveChangesAsync();
+            return Ok(result);
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteUnit(int id)
+        {
+            var result = await _context.Units.FirstOrDefaultAsync(u => u.Id == id);
+
+            if (result == null)
+            {
+                return NotFound("Unit with the given Id does not exits");
+            }
+
+            _context.Units.Remove(result);
+
+            await _context.SaveChangesAsync();
+            return Ok(await _context.Units.ToListAsync());
+
+        }
+
 
     }
 }
